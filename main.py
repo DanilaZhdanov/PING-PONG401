6e1cb5a8448f020c6c2b75bd08cf157435f0474a
DanilaZhdanov <154739326+DanilaZhdanov@users.noreply.github.com>
2024-01-21 15:40:42 +0300

    ДОБАВЛЕНИЕ ДВИЖЕНИЯ

--- main.py
+++ main.py
@@ -15,21 +15,21 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
-        if keys[k_UP] and self.rect.y>5:
+        if keys[K_UP] and self.rect.y>5:
             self.rect.y -= self.speed
-        if keys [k_DOWN] and self.rect.y < win_height - 80:
+        if keys [K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
-class Player(GameSprite):
+
     def update_l(self):
         keys = key.get_pressed()
-        if keys[k_w] and self.rect.y>5:
+        if keys[K_w] and self.rect.y>5:
             self.rect.y -= self.speed
-        if keys [k_s] and self.rect.y < win_height - 80:
+        if keys [K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
-racket1 = Player('tennis-clipart-design-illustration-free-png.png', 30, 20, 180, 50, 150)
-racket2 = Player('tennis-clipart-design-illustration-free-png.png', 520, 535, 180, 50, 150)
+racket1 = Player('tennis-clipart-design-illustration-free-png.png', 10, 20, 180, 50, 150)
+racket2 = Player('tennis-clipart-design-illustration-free-png.png', 10, 535, 180, 50, 150)
 ball = Player('explosive-bomb-black-png.png', 200, 260, 180, 50, 50)
 
 back = (23, 107, 99)
@@ -43,21 +43,42 @@ FPS = 61
 finish = False
 game = True
 
+speed_x = 3
+speed_y = 3
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('ИГРОК 1 НЕ УСЛЕДИЛ ЗА МЯЧОМ(', True, (238, 249, 109))
+lose2 = font.render('ИГРОК 2 НЕ УСЛЕДИЛ ЗА МЯЧОМ(', True, (238, 249, 109))
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
     if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+        if ball.rect.y < 0 or ball.rect.y > 450:
+            speed_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (100, 200))
+            game_over = True
+        if ball.rect.x > 600:
+            finish = True
+            window.blit(lose2, (100, 200))
+            game_over = True
+
         racket1.reset()
         racket2.reset()
         ball.reset()
 
     display.update()
     clock.tick(FPS)
-
-
-
-
-
-
